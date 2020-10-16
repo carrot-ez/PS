c21e37cf514e66b6e76313925344e79c9018a065
CarrotNuts <kain6245@gmail.com>
2020-10-16 20:39:34 +0900

    Add LongestSubSequence, dp

---
+++ Java/src/Baekjoon/LongestSubSequence.java
@@ -0,0 +1,38 @@
+package Baekjoon;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LongestSubSequence {
+    public static void main(String[] args) throws IOException {
+        // input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] splits = br.readLine().split(" ");
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+
+        for(int i=0; i < N; i++) {
+            dp[i] = 1;
+            arr[i] = Integer.parseInt(splits[i]);
+            if(i == 0)
+                continue;
+            for(int k=0; k<i; k++) {
+                if(arr[k] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[k] + 1);
+                }
+            }
+        }
+
+        int max = 0;
+        for(int i=0; i<N; i++) {
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+
+    }
+}
