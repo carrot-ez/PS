c5031dab937e6db68a89f5b4508df28e22db284b
CarrotNuts <kain6245@gmail.com>
2021-07-16 15:01:21 +0900

    가르침

--- Java/src/Baekjoon/가르침.java
+++ Java/src/Baekjoon/가르침.java
@@ -6,21 +6,21 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class 가르침 {
-    private static PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+    private static int max = Integer.MIN_VALUE;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] splits = br.readLine().split(" ");
-        int answer = 0;
 
-        int N = Integer.parseInt(splits[0]);
-        int K = Integer.parseInt(splits[1]);
+        int N = Integer.parseInt(splits[0]); // 배울 수 있는 단어 수
+        int K = Integer.parseInt(splits[1]); // 배울 수 있는 글자 제한
 
         List<String> contents = new ArrayList<>();
 
         while(N-- > 0) {
-            contents.add(br.readLine());
+            String s = br.readLine();
+            contents.add(s.substring(4, s.length() - 4));
         }
 
         System.out.println(solution(contents, K));
@@ -28,31 +28,30 @@ public class 가르침 {
 
     public static int solution(List<String> contents, int k) {
 
-        boolean[] alphabets = new boolean[26];
-        boolean[] fixed = new boolean[26];
-
-        // a, n, t, i, c 고정
-        alphabets[0] = fixed[0] = true; // a
-        alphabets[2] = fixed[2] = true; // c
-        alphabets[8] = fixed[8] = true; // i
-        alphabets[13] = fixed[13] = true; // n
-        alphabets[19] = fixed[19] = true; // t
-
-        int selectCnt = k - 5;
-
-        // can't make
-        if (selectCnt < 0) {
+        // terminate cond
+        if (k < 5) {
             return 0;
+        } else if (k == 26) {
+            return contents.size();
         }
 
+        boolean[] alphabets = new boolean[26];
+
+        // a, n, t, i, c 고정
+        alphabets[0] = true; // a
+        alphabets[2] = true; // c
+        alphabets[8] = true; // i
+        alphabets[13] = true; // n
+        alphabets[19] = true; // t
+
         // start back tracking
-        countReadable(selectCnt, alphabets, contents);
+        countReadable(0, k - 5, alphabets, contents);
 
 
-        return pq.peek();
+        return max;
     }
 
-    public static void countReadable(int cnt, boolean[] alphabets, List<String> contents) {
+    public static void countReadable(int idx, int cnt, boolean[] alphabets, List<String> contents) {
 
         if (cnt == 0) {
             // calc
@@ -72,16 +71,16 @@ public class 가르침 {
                 }
             }
 
-            pq.offer(readableCnt);
+            max = Math.max(max, readableCnt);
         }
         else {
-            for (int i = 0; i < 26; i++) {
+            for (int i = idx; i < 26; i++) {
                 if (alphabets[i]) {
                     continue;
                 }
 
                 alphabets[i] = true;
-                countReadable(cnt - 1, alphabets, contents);
+                countReadable(i + 1, cnt - 1, alphabets, contents);
                 alphabets[i] = false;
             }
         }
