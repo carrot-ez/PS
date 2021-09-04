5c944d2349128484a87405cef0a99af16f49daad
CarrotNuts <kain6245@gmail.com>
2021-09-04 14:26:41 +0900

    음양 더하기

---
+++ Java/src/Programmers/음양_더하기.java
@@ -0,0 +1,21 @@
+package Programmers;
+
+public class 음양_더하기 {
+
+    class Solution {
+        public int solution(int[] absolutes, boolean[] signs) {
+
+            int sum = 0;
+
+            for (int i = 0; i < absolutes.length; i++) {
+                if (signs[i]) {
+                    sum += absolutes[i];
+                } else {
+                    sum -= absolutes[i];
+                }
+            }
+
+            return sum;
+        }
+    }
+}
