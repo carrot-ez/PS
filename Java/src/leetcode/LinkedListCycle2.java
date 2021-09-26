c8e5fa2d32098b320558dcf1fbc8ce7579f21c60
CarrotNuts <kain6245@gmail.com>
2021-09-26 21:26:01 +0900

    Linked List Cycle 2

---
+++ Java/src/leetcode/LinkedListCycle2.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LinkedListCycle2 {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    class Solution {
+
+        Map<ListNode, Boolean> map = new HashMap<>();
+
+        public ListNode detectCycle(ListNode head) {
+
+            if (head == null) {
+                return null;
+            }
+
+            map.put(head, true);
+
+            while (head.next != null && !map.containsKey(head.next)) {
+                head = head.next;
+                map.put(head, true);
+            }
+
+            return head.next;
+        }
+    }
+
+}
