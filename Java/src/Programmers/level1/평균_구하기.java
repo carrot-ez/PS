01f84514d5b3a95adc87189502dbc1f2825cada9
CarrotNuts <kain6245@gmail.com>
2021-12-02 22:32:01 +0900

    평균 구하기

---
+++ Java/src/Programmers/level1/평균_구하기.java
@@ -0,0 +1,12 @@
+package Programmers.level1;
+
+import java.util.Arrays;
+
+public class 평균_구하기 {
+
+    public double solution(int[] arr) {
+        return Arrays.stream(arr)
+                .average()
+                .getAsDouble();
+    }
+}
