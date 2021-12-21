d19f06d060f730ecc69fdb0a0068c6ad1535206b
CarrotNuts <kain6245@gmail.com>
2021-12-22 02:06:46 +0900

    행렬 테두리 회전하기

---
+++ Java/src/Programmers/level2/행렬_테두리_회전하기.java
@@ -0,0 +1,68 @@
+package Programmers.level2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 행렬_테두리_회전하기 {
+    private int[][] board;
+    public int[] solution(int rows, int columns, int[][] queries) {
+
+        List<Integer> answer = new ArrayList<>();
+        board = new int[rows][columns];
+        initBoard(rows, columns);
+
+        for (int[] query : queries) {
+            int min = rotateBoardAndGetMin(query);
+            answer.add(min);
+        }
+
+        return answer.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+
+    public void initBoard(int rows, int columns) {
+        int cnt = 1;
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < columns; col++) {
+                board[row][col] = cnt++;
+            }
+        }
+    }
+
+    public int rotateBoardAndGetMin(int[] query) {
+        int min = Integer.MAX_VALUE;
+        int x1 = query[0] - 1;
+        int y1 = query[1] - 1;
+        int x2 = query[2] - 1;
+        int y2 = query[3] - 1;
+
+        int next = board[x1][y1];
+        for (int i = y1 + 1; i <= y2; i++) {
+            int tmp = board[x1][i];
+            board[x1][i] = next;
+            next = tmp;
+            min = Math.min(min, board[x1][i]);
+        }
+        for (int i = x1 + 1; i <= x2; i++) {
+            int tmp = board[i][y2];
+            board[i][y2] = next;
+            next = tmp;
+            min = Math.min(min, board[i][y2]);
+        }
+        for (int i = y2 - 1; i >= y1; i--) {
+            int tmp = board[x2][i];
+            board[x2][i] = next;
+            next = tmp;
+            min = Math.min(min, board[x2][i]);
+        }
+        for (int i = x2 - 1; i >= x1; i--) {
+            int tmp = board[i][y1];
+            board[i][y1] = next;
+            next = tmp;
+            min = Math.min(min, board[i][y1]);
+        }
+
+        return min;
+    }
+}
